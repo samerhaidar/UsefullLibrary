dc8dad54875c71b66549036992aaadddf58bda43
Samer Haidar <it.shaidar@gmail.com>
2018-03-24 13:09:07 -0700

    Adding New Project
    Adding New Project

---
+++ Extensions/Extensions/EvaluateAge.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extensions
+{
+    public static class EvaluateAge
+    {
+        public static int CalculateAge(this DateTime dateTime)
+        {
+            var age = DateTime.Now.Year - dateTime.Year;
+            if (DateTime.Now < dateTime.AddYears(age))
+                age--;
+            return age;
+        }
+    }
+}
---
+++ Extensions/Extensions/TestProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extensions
+{
+    public class TestProgram
+    {
+        static void Main(string[] args)
+        {
+            var age = new DateTime(1990, 09, 23).CalculateAge();
+      
+            Console.WriteLine(age);
+        }
+    }
+}
